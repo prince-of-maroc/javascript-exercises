e3a67343407e2148f63e95e5142cd77be831a423
Dara <jesprikovic@gmail.com>
2022-06-11 22:31:07 -0400

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let length = string.length;
+    let reversed = '';
+    for(i = length-1; i >= 0; i--){
+        reversed += string[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line

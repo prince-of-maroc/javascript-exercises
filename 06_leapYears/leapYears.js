e73fedd06615ef1d77ca26408e4b715927139060
Dara <jesprikovic@gmail.com>
2022-06-12 23:05:43 -0400

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,7 @@
-const leapYears = function() {
-
+const leapYears = (year) => {
+    if((year % 100 == 0) && (year % 400 != 0)) return false;
+    else if (year % 4 == 0) return true;
+    else return false;
 };
 
 // Do not edit below this line

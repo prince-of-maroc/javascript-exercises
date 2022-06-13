98c70a2f793ae5ac610945bc2383d1e3769989ef
Dara <jesprikovic@gmail.com>
2022-06-12 23:26:02 -0400

    Complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,5 @@
-const ftoc = function() {
-
-};
-
-const ctof = function() {
-
-};
+const ftoc = (celsius) => parseFloat(((celsius - 32) * 0.555555556).toFixed(1));
+const ctof = (f) => parseFloat((f * 1.8 + 32).toFixed(1));
 
 // Do not edit below this line
 module.exports = {
